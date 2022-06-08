06c11871d1fcc533feb1c8819fb98afcc2a0a701
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-06-08 19:17:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,19 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::vector<char> g;
+  std::string perm = tree.gPermut(n);
+  if (perm.length() == 0) {
+    return g;
+  }
+  g.resize(perm.length(), ' ');
+  int a = 0;
+  while (true) {
+    if (perm.length() == 0) {
+      return g;
+    } else {
+      g[a++] = perm[0];
+      perm.erase(perm.begin());
+    }
+  }
 }
